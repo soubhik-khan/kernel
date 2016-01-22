5100959927460552ffa97239b25aff87eddafa1a
brymaster5000 <brymaster5000@gmail.com>
2016-01-21 22:02:49 -0500

    tomato: Add & Enable PMC governor Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -440,6 +440,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PMC)
+extern struct cpufreq_governor cpufreq_gov_pmc;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pmc)
 #endif
 
 /*********************************************************************
