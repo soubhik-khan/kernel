a87825cfcbc2277fac4c957b6b9f214134b54a5d
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2016-06-15 20:37:21 +0530

    Fix Derps Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- lib/string.c
+++ lib/string.c
@@ -598,15 +598,7 @@ EXPORT_SYMBOL(memset);
 void memzero_explicit(void *s, size_t count)
 {
 	memset(s, 0, count);
-<<<<<<< HEAD
-<<<<<<< HEAD
 	barrier();
-=======
-	OPTIMIZER_HIDE_VAR(s);
->>>>>>> 21411c7... random: add and use memzero_explicit() for clearing data
-=======
-	barrier();
->>>>>>> 2cc70a1... lib: memzero_explicit: use barrier instead of OPTIMIZER_HIDE_VAR
 }
 EXPORT_SYMBOL(memzero_explicit);
 
